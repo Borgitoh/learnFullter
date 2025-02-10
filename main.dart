b743ea5591c619b46b28145ac3324ba1ca4cb69c
Kene Conde <76479531+Borgitoh@users.noreply.github.com>
2025-02-10 09:30:17 +0100

    imagem

--- main.dart
+++ main.dart
@@ -1,20 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main(){
   runApp(MaterialApp(
-      home: Scaffold(
-    appBar: AppBar(title: Text("primeiro App")),
-    body: Container(
-      color: Colors.black,
-      height: 250.0,
-      width: 250.0,
-      child: Column( 
-        mainAxisAlignment: MainAxisAlignment.center,
-        children:[
-              Container(height:50.0, width: 50.0, color: Colors.amber),
-              Container(height:50.0, width: 50.0, color: Colors.red),
-              Container(height:50.0, width: 50.0, color: Colors.green),
-                    ]),
-    ),
-  )));
+    home: Scaffold(
+     appBar: AppBar(
+       title: Text('Primeiro App'),
+       ),
+       body: Image.network('https://img.odcdn.com.br/wp-content/uploads/2024/05/shutterstock_2261596665-1.jpg'
+                          ,fit: BoxFit.contain , height: 1000.0)
+    )));
 }
